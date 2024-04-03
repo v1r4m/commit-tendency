64db51c9d2dc92ec570a4130ad9078675d116b56
v1r4m <choyoung7789@gmail.com>
2024-04-03 16:33:02 +0900

    이렇게 하는게 맞을까요....

--- commit-tendency/src/app/github/route.tsx
+++ commit-tendency/src/app/github/route.tsx
@@ -3,5 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     //get url param
     const code = request.nextUrl.searchParams.get("code");
-    return NextResponse.json({"code":code});
+    return new NextResponse(
+        `
+            <h1>${code}</h1>
+            <h2>dashboard header</h2>
+            <p>dashboard body</p>
+        `,
+        { status: 410, headers: { 'content-type': 'text/html' } }
+    );
 }
