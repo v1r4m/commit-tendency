0d55846b99370da3d18e3a1ee7da2aeabb5babda
v1r4m <choyoung7789@gmail.com>
2024-04-08 16:16:14 +0900

    토큰 받아오기

--- commit-tendency/src/app/github/route.tsx
+++ commit-tendency/src/app/github/route.tsx
@@ -3,12 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     //get url param
     const code = request.nextUrl.searchParams.get("code");
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "client_id": process.env.GITHUB_CLIENT_ID_LOCAL,
+      "client_secret": process.env.GITHUB_CLIENT_SECRET_LOCAL,
+      "code": code
+    });
+    
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw
+    };
+    
+    const tokenResponse = await fetch("https://github.com/login/oauth/access_token", requestOptions);
+    const response = await tokenResponse.text();
+
+    //get headers from tokenResponse
     return new NextResponse(
         `
-            <h1>${code}</h1>
+            <h1>${response}</h1>
             <h2>dashboard header</h2>
             <p>dashboard body</p>
         `,
-        { status: 410, headers: { 'content-type': 'text/html' } }
+        { status: 200, headers: { 'content-type': 'text/html' } }
     );
 }
